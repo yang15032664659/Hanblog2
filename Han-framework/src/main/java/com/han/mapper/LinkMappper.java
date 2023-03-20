1f0f94b14c8ebb771884ad4128efe523b7ee0252
杨锐涵 <1533096360@qq.com>
2023-03-20 15:28:04 +0800

    项目完成

--- Han-framework/src/main/java/com/han/mapper/LinkMappper.java
+++
@@ -1,9 +0,0 @@
-package com.han.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.han.entity.Link;
-import org.springframework.stereotype.Repository;
-
-@Repository//代表持久层
-public interface LinkMappper extends BaseMapper<Link> {
-}
--- Han-framework/src/main/java/com/han/mapper/RoleMenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.han.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.han.entity.RoleMenu;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleMenuMapper extends BaseMapper<RoleMenu> {
-}
